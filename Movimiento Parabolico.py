cde1a3b572d75d742c6a2f39736089b212d2d6f4
Reywood06 <97995461+Reywood06@users.noreply.github.com>
2023-02-26 22:17:34 -0500

    Nuevo Avance
    xd

--- Movimiento Parabolico.py
+++ Movimiento Parabolico.py
@@ -1,16 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import tkinter as tk
-from tkinter import ttk
+import matplotlib.ticker as mtl
+from tkinter import Tk, Frame, Button, Label, Entry, ttk, StringVar
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import matplotlib.animation as animation
+
 #Definimos los paramentros
 
-root = tk.Tk()
-root.config(width=725, height=525)
-theta=np.pi/4
+grafica, ax = plt.subplots(facecolor='#05FFBF')
+plt.title("Grafica de Movimiento Parabolico")
+
+theta=np.pi/3
 x0=0.
 y0=0.
-v0=50.
+v0=30.
 g=9.8
 
 def x_pos(theta,t,v0,x0):
@@ -27,12 +30,36 @@ N=len(t)
 
 fig, ax=plt.subplots()
 ln, = plt.plot(x,y,'ro')
-ax.set_xlim(0,280)
+ax.set_xlim(0,100)
+ax.xaxis.set_major_locator(mtl.MultipleLocator(10))
 ax.set_ylim(0,100)
+ax.yaxis.set_major_locator(mtl.MultipleLocator(10))
 
 def actualizar(i):
     ln.set_data(x[i],y[i])
     return ln,
 ani = animation.FuncAnimation(fig,actualizar,range(N),interval=0.00001)
-plt.grid()
-plt.show()
+
+def distancia(num):
+    num = 0
+
+
+MiVentana = Tk()
+MiVentana.geometry('725x525')
+MiVentana.title("Programa")
+MiVentana.minsize(width=725 , height=525)
+MiVentana.config(bg="white")
+
+frame = Frame(MiVentana, bd=3)
+frame.pack(expand=1, fill='both')
+
+canva = FigureCanvasTkAgg(fig, master = frame)
+canva.get_tk_widget().pack(padx=5, pady=5, expand=1, fill="both")
+
+entrada = StringVar()
+
+Label(frame, text='Ingrese la distancia: ', width=15, bg='white', fg='black').pack(pady = 5, side='left', expand=1)
+Entry(frame, width=35, bg = 'white', fg ='black', textvariable=entrada).pack(pady = 5, side='left', expand=1)
+Button(frame, text='Ingresar Dato', width=15, bg='black', fg='white').pack(pady = 5, side='left', expand=1)
+
+MiVentana.mainloop()
